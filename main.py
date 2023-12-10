0a87c54555fb4ff4d36a05fcd95a084267de408a
Adewunmi Aboyade-cole <67490387+geekwunmi@users.noreply.github.com>
2023-12-10 23:20:06 +0000

    updates from ade

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
-import jikanpy as jik
-import pandas as pd
-
-# anime = mal.Anime(1)
-# print(anime.score)
-
-# anime_title_df = pd.DataFrame(eval(str(mal.Anime.list)))
-# print(str(mal.AnimeSearchResult("Deadman ")))
-
-mushishi = jik.Jikan().anime(id=457,extension='characters')
-
-# print(mushishi)
-# print(mushishi.keys())
-# print(mushishi.get('data'))
+# import jikanpy as jik
+# import pandas as pd
+# import requests as re
 #
-mushishi_df = pd.DataFrame(mushishi.get('data'))
-# print(mushishi_df.columns)
-# mushishi_df = mushishi_df[['mal_id','title','title_japanese','aired','score']]
-# print(mushishi_df.head())
-jik.Jikan().anime(extension='Ani')
+# # anime = mal.Anime(1)
+# # print(anime.score)
+#
+# # anime_title_df = pd.DataFrame(eval(str(mal.Anime.list)))
+# # print(str(mal.AnimeSearchResult("Deadman ")))
+#
+# mushishi = jik.Jikan().anime(id=457,extension='characters')
+#
+# # print(mushishi)
+# # print(mushishi.keys())
+# # print(mushishi.get('data'))
+# #
+# mushishi_df = pd.DataFrame(mushishi.get('data'))
+# # print(mushishi_df.columns)
+# # mushishi_df = mushishi_df[['mal_id','title','title_japanese','aired','score']]
+# # print(mushishi_df.head())
+# jik.Jikan().anime(extension='Ani')
+#
+# # requests.get()
+#
+# I have zero hope that this will work but, hey, here's my attempt
+#First thing to note: When using the code, ensure that you have the requests library installed (can be installed via pip install requests).
+
+# Hope this helps even a little bit. If not, welp, I tried.
+
+import requests
+
+#Make a request to the API
+response = requests.get("https://api.jikan.moe/v3/anime/28223")  # 28223 is the ID for Death Parade
 
 
+# Parse the JSON response
 
+data = response.json()
+
+# Extract the required information
+name = data['title']
+score = data['score']
+genre = ', '.join(data['genres'][i]['name'] for i in range(len(data['genres'])))
+characters = ', '.join(character['name'] for character in data['characters'])
+studio = data['studios'][0]['name']
+episodes = data['episodes']
+
+# Print the results
+print(f"Name: {name}")
+print(f"Score: {score}")
+print(f"Genre: {genre}")
+print(f"Characters: {characters}")
+print(f"Studio: {studio}")
+print(f"Episodes: {episodes}")
 
 
--- reader.py
+++ reader.py
@@ -10,6 +10,13 @@ def getGenresAsString(genres):
 
   return ", ".join(genreNames)
 
+def getProducersFromDict(producers): # this isn't work yet lolz i dont know how to maniplate dictionaries and sstrongs yet
+  for index,producer in enumerate(producers) :
+    if index == "name":
+      return producer
+    else:
+      pass
+
 def main():
   animeList = []
 
@@ -24,9 +31,12 @@ def main():
     "",
     1,
     {
-      "limit": 10,
-      "order_by": "rank",
-      "sort": "desc"
+      "limit": 20,
+      "order_by": "score",
+      "sort": "desc",
+      "start_date": "2020-01-01",
+      "type":"tv",
+      "status":"complete"
     }
   )
 
@@ -41,11 +51,13 @@ def main():
         "id": item["mal_id"],
         "name": item["title"],
         "season": item["season"], # Need to confirm what this is. Is it season as in Summer, Fall, etc. Or season as in number of seasons. Not sure the API provides that.
-        "rating": item["rating"], # e.g: PG - Children
+        "score": item["score"], # e.g: PG - Children
         "year": item["year"], # can be None, maybe ["aired"]["from"] date can be used instead by extracting the year part of the date if "year" is None?
         "link": item["url"],
         "image": item["images"]["jpg"]["image_url"],
-        "genre": getGenresAsString(item["genres"])
+        "genre": getGenresAsString(item["genres"]),
+        "producers":getProducersFromDict(item["producers"])
+        # "moreinfo":item["moreinfo"]
       }
 
       animeList.append(anime)
@@ -54,9 +66,12 @@ def main():
   currentTimeStamp = time.time()
   sheetName = "animeList" + str(currentTimeStamp) + ".xlsx"
   dataFrame = pd.DataFrame(data=animeList)
-  dataFrame.to_excel(sheetName, index=False)
 
-  print("Data exported into", sheetName)
+  pd.set_option("display.max_columns",10)
+  print(dataFrame.head(10))
 
+  # dataFrame.to_excel(sheetName, index=False)
 
+  # print("Data exported into", sheetName)
 main()
+
