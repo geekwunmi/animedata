ed5082750e469b0e19610fb3ba079fcdcb1e7572
Adewunmi Aboyade-cole <67490387+geekwunmi@users.noreply.github.com>
2023-12-17 00:48:32 +0000

    updated changes effected

--- reader.py
+++ reader.py
@@ -2,112 +2,117 @@ from jikanpy import Jikan
 import pandas as pd
 import time
 
+
 def getGenresAsString(genres):
-  genreNames = []
+    genreNames = []
 
-  for genre in genres:
-    genreNames.append(genre["name"])
+    for genre in genres:
+        genreNames.append(genre["name"])
 
-  return ", ".join(genreNames)
+    return ", ".join(genreNames)
+
 
 def getProducersFromDict(producers):
-  producerNames = []
+    producerNames = []
 
-  for producer in producers:
-    name = producer["name"]
-    producerNames.append(name)
+    for producer in producers:
+        name = producer["name"]
+        producerNames.append(name)
 
-  return ", ".join(producerNames)
+    return ", ".join(producerNames)
 
 
 def fetchData(page: int, limit: int):
-  jikan = Jikan()
+    jikan = Jikan()
 
-  print("Fetching data from Jikan... Page:", page)
-  result = jikan.search(
-    "anime",
-    "",
-    page,
-    {
-      "limit": limit,
-      "order_by": "score",
-      "sort": "desc",
-      "start_date": "2020-01-01",
-      "type": "tv",
-      "status": "complete",
-      "min_score": "6"
-    }
-  )
+    print("Fetching data from Jikan... Page:", page)
+    result = jikan.search(
+        "anime",
+        "",
+        page,
+        {
+            "limit": limit,
+            "order_by": "score",
+            "sort": "desc",
+            "start_date": "2020-01-01",
+            "type": "tv",
+            "status": "complete",
+            "min_score": "6"
+        }
+    )
 
-  return result
+    return result
+
 
 def appendResultToList(animeList: list[any], data: dict[str, any]):
-  for item in data:
-    anime = {
-      "id": item["mal_id"],
-      "name": item["title"],
-      "season": item["season"], # Need to confirm what this is. Is it season as in Summer, Fall, etc. Or season as in number of seasons. Not sure the API provides that.
-      "score": item["score"], # e.g: PG - Children
-      "year": item["year"], # can be None, maybe ["aired"]["from"] date can be used instead by extracting the year part of the date if "year" is None?
-      "link": item["url"],
-      "image": item["images"]["jpg"]["image_url"],
-      "genre": getGenresAsString(item["genres"]),
-      "producers": getProducersFromDict(item["producers"]),
-      "members": item["members"]
-      # "moreinfo":item["moreinfo"]
-    }
+    for item in data:
+        anime = {
+            "id": item["mal_id"],
+            "name": item["title"],
+            "season": item["season"],
+            # Need to confirm what this is. Is it season as in Summer, Fall, etc. Or season as in number of seasons. Not sure the API provides that.
+            "score": item["score"],  # e.g: PG - Children
+            "year": item["year"],
+            # can be None, maybe ["aired"]["from"] date can be used instead by extracting the year part of the date if "year" is None?
+            "link": item["url"],
+            "image": item["images"]["jpg"]["image_url"],
+            "genre": getGenresAsString(item["genres"]),
+            "producers": getProducersFromDict(item["producers"]),
+            "members": item["members"]
+            # "moreinfo":item["moreinfo"]
+        }
 
-    animeList.append(anime)
-  
-  return animeList
+        animeList.append(anime)
+
+    return animeList
 
 
 def main():
-  limit = 20
-  page = 1
-  requestCount = 0
+    limit = 20
+    page = 1
+    requestCount = 0
 
-  # fetch first set of data
-  result = fetchData(page, limit)
-  requestCount += 1
-
-  if len(result["data"]) == 0:
-    print("No data returned from Jikan API...")
-    return
-  
-  animeList = appendResultToList([], result["data"])
-
-  # get total size of result to determine the number of requests to fetch remaining records
-  totalSizeFromApi = result["pagination"]["items"]["total"]
-  print("Total size:", totalSizeFromApi, "fetching", limit, "per page")
-
-  # reduce totalSizeFromApi by the limit since we've fetched the first set
-  totalSizeFromApi -= limit
-
-  while totalSizeFromApi > 0:
-    # increment page and fetch next set
-    page += 1
+    # fetch first set of data
     result = fetchData(page, limit)
     requestCount += 1
 
-    animeList = appendResultToList(animeList, result["data"])
+    if len(result["data"]) == 0:
+        print("No data returned from Jikan API...")
+        return
 
-    # again, reduce the totalSizeFromApi by the limit since we've fetched another set
+    animeList = appendResultToList([], result["data"])
+
+    # get total size of result to determine the number of requests to fetch remaining records
+    totalSizeFromApi = result["pagination"]["items"]["total"]
+    print("Total size:", totalSizeFromApi, "fetching", limit, "per page")
+
+    # reduce totalSizeFromApi by the limit since we've fetched the first set
     totalSizeFromApi -= limit
 
-    if requestCount == 25:
-      # pause for a minute in order not to hit the API rate limit and reset requestCount
-      print("Sleeping for 60 seconds... zzzzz...")
-      time.sleep(60)
+    while totalSizeFromApi > 0:
+        # increment page and fetch next set
+        page += 1
+        result = fetchData(page, limit)
+        requestCount += 1
 
-      print("Waking up and resuming request...")
-      requestCount = 0
+        animeList = appendResultToList(animeList, result["data"])
 
-  dataFrame = pd.DataFrame(data=animeList)
-  pd.set_option("display.max_columns",10)
-  print(dataFrame.head(10))
+        # again, reduce the totalSizeFromApi by the limit since we've fetched another set
+        totalSizeFromApi -= limit
 
-  return
+        if requestCount == 25:
+            # pause for a minute in order not to hit the API rate limit and reset requestCount
+            print("Sleeping for 60 seconds... zzzzz...")
+            time.sleep(60)
+
+            print("Waking up and resuming request...")
+            requestCount = 0
+
+    dataFrame = pd.DataFrame(data=animeList)
+    pd.set_option("display.max_columns", 10)
+    print(dataFrame.head(10))
+
+    return
+
 
 main()
-
